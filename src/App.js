dfc235ba6f31dcfcfcf3d4a7acfd65334025e9d2
Keith Bender <k.bender30@gmail.com>
2019-08-15 21:46:28 -0500

    finished implementation on homepage, as well as the styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/hompage.component'
+
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return(
+      <div>
+        <HomePage />
+      </div>
+    )
   }
 }
 
